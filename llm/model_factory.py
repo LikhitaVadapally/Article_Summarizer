ed384274fee348c8beac09716e904b8b7f867dc3
LikhitaVadapally <anandlikhita@gmail.com>
2025-09-23 11:26:26 +0530

    Set ChatOllama num_ctx=4000 for faster inference

--- llm/model_factory.py
+++ llm/model_factory.py
@@ -9,19 +9,20 @@ load_dotenv()
 
 Provider = Literal["openai", "ollama"]
 
+#factory func
 def get_chat_model(
     provider: Provider,
     model_name: Optional[str] = None,
     temperature: float = 0.0,
 ):
     provider = (provider or os.getenv("DEFAULT_PROVIDER", "ollama")).lower()
-
+#If no provider is passed, fallback to default ollama
     if provider == "openai":
         model = model_name or os.getenv("DEFAULT_OPENAI_MODEL", "gpt-4o-mini")
         return ChatOpenAI(model=model, temperature=temperature)
     elif provider == "ollama":
         model = model_name or os.getenv("DEFAULT_OLLAMA_MODEL", "llama3.2:3b")
         return ChatOllama(model=model, temperature=temperature, model_kwargs={"num_ctx": 4000})
-    #default might be 4096 tokens or more
+    
     else:
         raise ValueError(f"Unknown provider: {provider}")
